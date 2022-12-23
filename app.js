86ee12c9fd49ba1acadf086f3b36318e12899918
Kacper Stępień <kacper2007x48@gmail.com>
2022-12-24 00:54:17 +0100

    add index.js, register.js and create styles

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+require('dotenv').config(); // Dzięki temu możemy używać zmiennych środowiskowych z pliku .env
+const path = require('path');   // Dzięki temu możemy używać ścieżek do plików
+
+const app = express();
+const port = process.env.PORT;
+app.use(express.static('public'));  // Dzięki temu możemy używać plików statycznych z folderu public
+app.use("/js", express.static(__dirname + "/public/js"));
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/views', express.static(__dirname + '/views'));
+
+
+app.get('/', (req, res) => {
+    res.send('Hello Express');
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
